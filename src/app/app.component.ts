297fb76ee967aad6d1c06858518a0fa7dfdb6b30
deborahjpliegog <debyneko15@gmail.com>
2025-11-22 20:28:18 -0600

    Sign-in y Sign-up diseño terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignInComponent } from './auth/features/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/features/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SignInComponent, SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

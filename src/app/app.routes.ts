55d3e7a0ddcdd98e06734f41d261d5606edb3c7b
IEVN1002-22001770 <emilio.neyvi@gmail.com>
2025-11-26 18:47:09 -0600

    Diseño de recuperar, configurar cuenta y verificacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignUpComponent } from './auth/features/sign-up/sign-up.component';
+import { RecuperarCuentaComponent } from './recuperar-cuenta/recuperar-cuenta.component';
+import { CodigoVerificacionComponent } from './codigo-verificacion/codigo-verificacion.component';
+import { ConfigCuentaComponent } from './config-cuenta/config-cuenta.component';
+
+export const routes: Routes = [
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: 'recuperar-cuenta',
+    component: RecuperarCuentaComponent
+  },
+  {
+    path: 'codigoveri',
+    component: CodigoVerificacionComponent
+  },
+  {
+    path: 'config-cuenta',
+    component: ConfigCuentaComponent
+  },
+
+
+  // (Opcional) Si entran a la raíz, redirigir al catálogo o a donde quieras
+  { path: '', redirectTo: 'catalogo', pathMatch: 'full' }
+];
+
